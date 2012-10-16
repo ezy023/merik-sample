272ca6212c23374764d114c68cbad53f73b5ab28
Erik <allareri@gmail.com>
2012-10-16 13:42:47 -0400

    Add s3 storage for music and images

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',       # required
-    :aws_access_key_id      => 'AKIAI4PFY5SQJCS2MFRA',       # required
-    :aws_secret_access_key  => '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',       # required
+    :aws_access_key_id      => ENV["S3_ACCESS_KEY"],       # required
+    :aws_secret_access_key  => ENV["S3_SECRET_KEY"],       # required
   }
-  config.fog_directory  = 'muzerik-songs'                     # required
+  config.fog_directory  = ENV["S3_BUCKET_SONGS"]                     # required
  end
