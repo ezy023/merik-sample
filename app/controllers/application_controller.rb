2cc61ba935ef59843a4572304fe429d8d3be23f5
Erik <allareri@gmail.com>
2012-10-16 10:11:51 -0400

    Replace http auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
 
 # adds authentication to make access to the app private
-# http_basic_authenticate_with :name => "allowed", password: "MasterChief"
+http_basic_authenticate_with :name => "allowed", password: "MasterChief"
 
   protect_from_forgery
   include SessionsHelper
