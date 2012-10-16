c4c4e3e3656f214fa831bbc7e56540c19e365372
Erik <allareri@gmail.com>
2012-10-16 12:16:45 -0400

    Add basic default image without scaling

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,10 +21,12 @@ class User < ActiveRecord::Base
   # has_many :reposts
   # has_many :microposts, :through => :reposts
 
+
   mount_uploader :image, ImageUploader
   
   before_save { |user| user.email = email.downcase }
   before_save :create_remember_token
+  
   
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121016014856) do
+ActiveRecord::Schema.define(:version => 20121016150243) do
 
   create_table "messages", :force => true do |t|
     t.datetime "created_at", :null => false
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(:version => 20121016014856) do
     t.string   "password_reset_token"
     t.datetime "password_reset_sent_at"
     t.string   "image"
+    t.string   "summary"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
