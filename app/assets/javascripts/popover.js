f722cceae9035313f3e5c153d8cd19a8146b6719
ezy023 <allareri@gmail.com>
2012-10-23 10:51:34 -0400

    Remove popover.js

--- app/assets/javascripts/popover.js
+++ app/assets/javascripts/popover.js
@@ -1,22 +1,22 @@
-$('document').ready(function(){
-     $('#post-button').popover({
-     	trigger: 'hover',
-     	title: 'Post a song',
-     	content: 'What are you listening to?'
-     },'show');
-     $('#search-popover').popover({
-     	placement: 'bottom',
-     	title: 'Search for music',
-     	content: 'Find some new tunes',
-     	trigger: 'hover'
-     }, 'show');
-     $('#users-popover').popover({
-     	placement: 'bottom',
-     	title: 'Search for people',
-     	content: 'Find and follow people who share your music interests',
-     	trigger: 'hover'
+// $('document').ready(function(){
+//      $('#post-button').popover({
+//      	trigger: 'hover',
+//      	title: 'Post a song',
+//      	content: 'What are you listening to?'
+//      },'show');
+//      $('#search-popover').popover({
+//      	placement: 'bottom',
+//      	title: 'Search for music',
+//      	content: 'Find some new tunes',
+//      	trigger: 'hover'
+//      }, 'show');
+//      $('#users-popover').popover({
+//      	placement: 'bottom',
+//      	title: 'Search for people',
+//      	content: 'Find and follow people who share your music interests',
+//      	trigger: 'hover'
 
-     }, 'show');
+//      }, 'show');
     	  
-});
+// });
 
