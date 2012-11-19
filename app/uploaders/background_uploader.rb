68b76dfeb9b986353dad82e2e792ab7a5a762442
ezy023 <allareri@gmail.com>
2012-11-19 14:09:53 -0500

    Change background_image storage to fog

--- app/uploaders/background_uploader.rb
+++ app/uploaders/background_uploader.rb
@@ -11,8 +11,8 @@ class BackgroundUploader < CarrierWave::Uploader::Base
   include Sprockets::Helpers::IsolatedHelper
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  # storage :file
+  storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -36,9 +36,9 @@ class BackgroundUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process :scale => [50, 50]
-  # end
+  version :preview do
+    process :resize_to_limit => [150, 150]
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
