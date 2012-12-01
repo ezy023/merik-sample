83e9205a262d12af6619873424d2e5c8cef24f38
ezy023 <allareri@gmail.com>
2012-12-01 16:42:51 -0500

    Basic styling for SC form, remove hashtag from form

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,9 +1,16 @@
 $(document).ready(function () {
-	$('#what-is-hashtag').tooltip({
+	// $('#what-is-hashtag').tooltip({
+	// 	placement: 'right',
+	// 	title: 'Track tweets and comments about this song through Twitter',
+
+	// });
+
+	$('#what-is-soundcloud').tooltip({
 		placement: 'right',
-		title: 'Track tweets and comments about this song through Twitter',
+		title: 'Post a link to a song (or playlist) on Soundcloud to share it with your followers',
 
 	});
+
 	$('.icon-play').hide()
 	$(".song-link").hover(function(){
 	  $(this).children('.icon-play').show();
