06609deb4bf40788b6f079b4fe208e472f21321d
ezy023 <allareri@gmail.com>
2012-12-21 14:01:29 -0500

    First round of suggested site design fixes, mainly static page consistency and sign-in

--- app/controllers/frequentlyaskedquestions_controller.rb
+++ app/controllers/frequentlyaskedquestions_controller.rb
@@ -2,7 +2,7 @@ class FrequentlyaskedquestionsController < ApplicationController
   def create
   	@faq = FrequentlyAskedQuestions.new(question: params[:question])
   	if @faq.save
-  		flash[:success] = "Thank you, we will answer this question ASAP"
+  		flash[:success] = "Question submitted"
   		redirect_to :back
   	end
   end
