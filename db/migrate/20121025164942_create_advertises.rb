71432d69412cf953400a27aeda6852f4afc51bde
ezy023 <allareri@gmail.com>
2012-12-23 13:46:50 -0500

    Remove deleted files

--- db/migrate/20121025164942_create_advertises.rb
+++
@@ -1,8 +0,0 @@
-class CreateAdvertises < ActiveRecord::Migration
-  def change
-    create_table :advertises do |t|
-
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20121101184304_add_accepted_terms_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddAcceptedTermsToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :accepted_terms, :boolean, :default => false
-  end
-end
